16fee8887e43ee47913764639fec63a71f0452ab
Obikaonu Samuel <samuelchibuike22@gmail.com>
2020-06-22 13:37:04 +0100

    feat: (comment, likes) added functionality to comment and like a post in  real time

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,52 @@
 <template>
+    <!-- post modal -->
+
     <div id="dashboard">
-        <section>
+      <transition name="fade">
+          <div v-if="hiddenPosts.length" @click="showNewPosts" class="hidden-posts">
+              <p>
+                  Click to show <span class="new-posts">{{ hiddenPosts.length }}</span> 
+                  new <span v-if="hiddenPosts.length > 1">posts</span><span v-else>post</span>
+              </p>
+          </div>
+      </transition>
+      <!-- comment modal -->
+      <transition name="fade">
+          <div v-if="showCommentModal" class="c-modal">
+              <div class="c-container">
+                  <a @click="closeCommentModal">X</a>
+                  <p>add a comment</p>
+                  <form @submit.prevent>
+                      <textarea v-model.trim="comment.content"></textarea>
+                      <button @click="addComment" :disabled="comment.content == ''" class="button">add comment</button>
+                  </form>
+              </div>
+          </div>
+      </transition>
+      <transition name="fade">
+    <div v-if="showPostModal" class="p-modal">
+        <div class="p-container">
+            <a @click="closePostModal" class="close">X</a>
+            <div class="post">
+                <h5>{{ fullPost.userName }}</h5>
+                <span>{{ fullPost.createdOn | formatDate }}</span>
+                <p>{{ fullPost.content }}</p>
+                    <ul>
+                        <li><a @click="openCommentModal(fullPost)">comments {{ fullPost.comments || '' }}</a></li>
+                        <li><a @click="likePost(fullPost.id, fullPost.likes)">likes {{ fullPost.likes || ''  }}</a></li>
+                    </ul>
+            </div>
+            <div v-show="postComments.length" class="comments">
+                <div v-for="(comment, index) in postComments" :key="index" class="comment">
+                    <p>{{ comment.userName }}</p>
+                    <span>{{ comment.createdOn | formatDate }}</span>
+                    <p>{{ comment.content }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</transition>
+        <section v-if="!showPostModal" >
             <div class="col1">
                 <div class="profile">
                     <h5>Welcome Back {{ userProfile.name }}</h5>
@@ -21,9 +67,9 @@
           <span>{{ post.createdOn | formatDate }}</span>
           <p>{{ post.content | trimLength }}</p>
           <ul>
-              <li><a>comments {{ post.comments }}</a></li>
-              <li><a>likes {{ post.likes }}</a></li>
-              <li><a>view full post</a></li>
+              <li><a @click="openCommentModal(post)">comments {{ post.comments || '' }}</a></li>
+              <li><a @click="likePost(post.id, post.likes)">likes {{ post.likes || ''  }}</a></li>
+              <li><a @click="viewPost(post)">view full post</a></li>
           </ul>
       </div>
   </div>
@@ -46,11 +92,21 @@ export default {
     return {
       post: {
         content: '',
-      }
+      },
+      comment: {
+            postId: '',
+            userId: '',
+            content: '',
+        },
+        showCommentModal: false,
+        showPostModal: false,
+        fullPost: {},
+        postComments: [],
+        onePostId: null,
     };
   },
   computed: {
-    ...mapState(['userProfile', 'currentUser', 'posts']),
+    ...mapState(['userProfile', 'currentUser', 'posts', 'hiddenPosts']),
   },
   methods: {
     createPost() {
@@ -66,7 +122,96 @@ export default {
           }).catch(err => {
               console.log(err)
           })
-        }   
+        },
+        showNewPosts(){
+          let updatePostsArray = this.hiddenPosts.concat(this.posts);
+
+          // clear hidden Posts array and update posts array
+          this.$store.commit('setHiddenPosts', null);
+          this.$store.commit('setPosts', updatePostsArray);
+
+        },
+        openCommentModal(post) {
+         this.comment.postId = post.id
+          this.comment.userId = post.userId
+          this.comment.postComments = post.comments
+          this.showCommentModal = true
+          },
+    closeCommentModal() {
+        this.comment.postId = ''
+        this.comment.userId = ''
+        this.comment.content = ''
+        this.showCommentModal = false
+         },
+    addComment() {
+        let postId = this.comment.postId
+        let postComments = this.comment.postComments
+        
+        fb.commentsCollection.add({
+            createdOn: new Date(),
+            content: this.comment.content,
+            postId: postId,
+            userId: this.currentUser.uid,
+            userName: this.userProfile.name
+        }).then(doc => {
+            fb.postsCollection.doc(postId).update({
+                comments: postComments + 1
+            }).then(() => {
+                this.closeCommentModal()
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+    },
+    likePost(postId, postLikes) {
+    let docId = `${this.currentUser.uid}_${postId}`
+    fb.likesCollection.doc(docId).get().then(doc => {
+        if (doc.exists) {
+          fb.likesCollection.doc(docId).delete()
+          .then(() => {
+            // update post likes
+            fb.postsCollection.doc(postId).update({
+                likes: postLikes - 1
+            })
+        })
+            return
+        }
+
+        fb.likesCollection.doc(docId).set({
+            postId: postId,
+            userId: this.currentUser.uid
+        }).then(() => {
+            // update post likes
+            fb.postsCollection.doc(postId).update({
+                likes: postLikes + 1
+            })
+        })
+        this.$store.commit('setCurrentUser', currentUser);
+    }).catch(err => {
+        console.log(err)
+    })
+},
+    viewPost(post) {
+        fb.commentsCollection.where('postId', '==', post.id).get().then(docs => {
+            let commentsArray = []
+
+            docs.forEach(doc => {
+                let comment = doc.data()
+                comment.id = doc.id
+                commentsArray.push(comment)
+            })
+
+            this.postComments = commentsArray
+            this.fullPost = post
+            this.showPostModal = true
+        }).catch(err => {
+            console.log(err)
+        })
+    },
+    closePostModal() {
+        this.postComments = []
+        this.showPostModal = false
+    }
 
   },
   filters: {
--- src/store.js
+++ src/store.js
@@ -9,18 +9,19 @@ export const store = new Vuex.Store({
     currentUser: null,
     userProfile: {},
     posts: [],
+    hiddenPosts: [],
   },
   actions: {
     clearData({ commit }) {
       commit('setCurrentUser', null);
       commit('setUserProfile', {});
       commit('setPosts', null);
+      commit('setHiddenPosts', null);
     },
 
     fetchUserProfile({ commit, state }) {
       console.log(state.currentUser);
       fb.usersCollection.doc(state.currentUser.uid).get().then((res) => {
-        console.log(res);
         commit('setUserProfile', res.data());
       }).catch((err) => {
         console.log(err);
@@ -35,27 +36,59 @@ export const store = new Vuex.Store({
       state.userProfile = val;
     },
     setPosts(state, val) {
-      state.posts = val;
+     if(val) {
+        state.posts = val;
+      } else{
+        state.posts = [];
+      }
     },
+    setHiddenPosts(state, val){
+     if(val) { 
+       if(!state.hiddenPosts.some(x => x.id == val.id)){
+        state.hiddenPosts.unshift(val);
+      }
+    }
+    else{
+      state.hiddenPosts = [];
+    }
+
+    }
   },
 });
 
-fb.auth.onAuthStateChanged((user) => {
+// handle page reload
+fb.auth.onAuthStateChanged(user => {
   if (user) {
-    store.commit('setCurrentUser', user);
-    store.dispatch('fetchUserProfile');
+      store.commit('setCurrentUser', user)
+      store.dispatch('fetchUserProfile')
 
-    // realtime updates from our posts collection
-    fb.postsCollection.orderBy('createdOn', 'desc').onSnapshot((querySnapshot) => {
-      const postsArray = [];
+      // realtime updates from our posts collection
+      fb.postsCollection.orderBy('createdOn', 'desc').onSnapshot(querySnapshot => {
+          // check if created by currentUser
+          let createdByCurrentUser
+          if (querySnapshot.docs.length) {
+              createdByCurrentUser = store.state.currentUser.uid == querySnapshot.docChanges()[0].doc.data().userId ? true : false
+          }
 
-      querySnapshot.forEach((doc) => {
-        const post = doc.data();
-        post.id = doc.id;
-        postsArray.push(post);
-      });
+          // add new posts to hiddenPosts array after initial load
+          if (querySnapshot.docChanges().length !== querySnapshot.docs.length 
+             && querySnapshot.docChanges()[0].type == 'added' && !createdByCurrentUser ) {
 
-      store.commit('setPosts', postsArray);
-    });
+              let post = querySnapshot.docChanges()[0].doc.data()
+              post.id = querySnapshot.docChanges()[0].doc.id
+
+              store.commit('setHiddenPosts', post)
+          } else {
+              let postsArray = []
+
+              querySnapshot.forEach(doc => {
+                  let post = doc.data()
+                  post.id = doc.id
+                  postsArray.push(post)
+              })
+
+              store.commit('setPosts', postsArray)
+          }
+      })
   }
-});
+})
